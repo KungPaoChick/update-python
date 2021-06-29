77f1709d0b3a890b064a58fa4499568191424ce8
KungPaoChick <matthewriel63@gmail.com>
2021-06-29 11:51:19 +0800

    simplified colorama lines

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import requests
+import os
+import wget
 import colorama
+from colorama import Fore, Style
 from bs4 import BeautifulSoup as soup
 
 
@@ -19,31 +22,24 @@ class Python_Update:
             for div in page_soup.findAll('div', {'class': 'download-os-windows'}):
                 for version in div.findAll('a', {'class': 'button'}):
                     release.extend((version.text.split()[-1], version['href']))
-                    print(colorama.Fore.GREEN, f'[*] Latest Release: {version.text.split()[-1]}',
-                        colorama.Style.RESET_ALL)
+                    print(Fore.GREEN, f'[*] Latest Release: {version.text.split()[-1]}', Style.RESET_ALL)
             
             if not release[0] == platform.python_version():
                 print(f'Current version: {platform.python_version()} - New Version: {release[0]}')
                 Python_Update(self.py_link).download_update(release[1])
             else:
-                print(colorama.Fore.YELLOW, '[!] Your Python version is up to date!',
-                    colorama.Style.RESET_ALL)
+                print(Fore.YELLOW, '[!] Your Python version is up to date!', Style.RESET_ALL)
         except requests.HTTPError as err:
-            print(colorama.Fore.RED,
-                f'[!!] Something went wrong! {err}', colorama.Style.RESET_ALL)
+            print(Fore.RED, f'[!!] Something went wrong! {err}', Style.RESET_ALL)
 
     def download_update(self, link):
-        import os
-        import wget
         try:
             wget.download(link, link.split('/')[-1])
             
             if link.split('/')[-1] in os.listdir(os.path.join(os.getcwd())):
-                print(colorama.Fore.GREEN, f"\n[*] {link.split('/')[-1]} has been downloaded",
-                    colorama.Style.RESET_ALL)
+                print(Fore.GREEN, f"\n[*] {link.split('/')[-1]} has been downloaded", Style.RESET_ALL)
         except Exception as err:
-            print(colorama.Fore.RED,
-                f'[!!] Something went wrong! {err}', colorama.Style.RESET_ALL)
+            print(Fore.RED, f'[!!] Something went wrong! {err}', Style.RESET_ALL)
 
 
 if __name__ == '__main__':
